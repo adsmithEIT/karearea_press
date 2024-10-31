91dfa81ea037a8c1641b268ccabe0a82ff94ea2e
adsmithEIT <ASmith@eit.ac.nz>
2024-11-01 11:52:09 +1300

    Add files via upload

--- data.js
+++ data.js
@@ -1,66 +1,31 @@
-//Normally data is stored in DB, when app sends a request to DB, it return this array of data
-//Link: https://www.imdb.com/chart/top/
-let items =[{id: 1, title: "The Shawshank Redemption", year: 1994, price: 7.50, category: 1, image_url: "Images/ItemImage.jpg", description: "When an innocent male banker is sent to prison accused of murdering his wife, he does everything that he can over the years to break free and escape from prison. While on the inside, he develops a friendship with a fellow inmate that could last for years."}, 
-    {id: 2, title: "The Godfather", year: 1972, price: 7.75, category: 2, image_url: "Images/ItemImage.jpg", description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."}, 
-    {id: 3, title: "The Dark Knight", year: 2008, price: 8.95, category: 3, image_url: "Images/ItemImage.jpg", description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness."}, 
-    {id: 4, title: "12 Angry Men", year: 1957, price: 5.95, category: 2, image_url: "Images/ItemImage.jpg", description: "The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict."}, 
-    {id: 5, title: "Schindler\'s List", year: 1993, price: 8.50, category: 1, image_url: "Images/ItemImage.jpg", description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis."}, 
-    {id: 6, title: "The Lord of the Rings: The Return of the King", year: 2003, price: 8.95, category: 4, image_url: "Images/ItemImage.jpg", description: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."}, 
-    {id: 7, title: "Pulp Fiction", year: 1994, price: 6.95, category: 4, image_url: "Images/ItemImage.jpg", description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."}, 
-    {id: 8, title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001, price: 10.95, category: 4, image_url: "Images/ItemImage.jpg", description: "Praesent vulputate, ligula ac vehicula consectetur, dui lectus feugiat tellus, rutrum hendrerit tellus mi nec lorem. Praesent vitae dapibus lectus."}, 
-    {id: 9, title: "The Good, the Bad and the Ugly", year: 1966, price: 7.50, category: 3, image_url: "Images/ItemImage.jpg", description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron."}, 
-    {id: 10, title: "Forrest Gump", year: 1994, price: 11.95, category: 3, image_url: "Images/ItemImage.jpg", description: "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart."},
-    {id: 11, title: "Fight Club", year: 1999, price: 12.95, category: 3, image_url: "Images/ItemImage.jpg", description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."},
-    {id: 12, title: "Inception", year: 2010, price: 7.95, category: 4, image_url: "Images/ItemImage.jpg", description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."},
-    {id: 13, title: "The Matrix", year: 1999, price: 9.95, category: 4, image_url: "Images/ItemImage.jpg", description: "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence."},
-    {id: 14, title: "Goodfellas", year: 1990, price: 5.95, category: 2, image_url: "Images/ItemImage.jpg", description: "The story of Henry Hill and his life in the mafia, covering his relationship with his wife Karen and his mob partners Jimmy Conway and Tommy DeVito."},
-    {id: 15, title: "One Flew Over the Cuckoo\'s Nest", year: 1975, price: 8.95, category: 1, image_url: "Images/ItemImage.jpg", description: "In the Fall of 1963, a Korean War veteran and criminal pleads insanity and is admitted to a mental institution, where he rallies up the scared patients against the tyrannical nurse."},
-    {id: 16, title: "Interstellar", year: 2014, price: 11.95, category: 4, image_url: "Images/ItemImage.jpg", description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans."}
-];
+//Array
+let items =[{id: 1, title: "Force of Nature: A Conservation History of Forest & Bird", author: "David Young & Naomi Arnold", year: 2024, price: 89.99, category: 1, image_url: "Images/Force-of-Nature-FB-Cover-600px-wide-72dpi.jpg", description: "The inspiring history of Forest & Bird, New Zealand's oldest independent conservation organisation for over 100 years. Beginning in 1923, Forest & Bird have been involved in all of the significant environmental campaigns of the last century, starting with efforts to protect native birds and their forest habitat, creation of many of the national parks and in recent decades efforts to protect our marine environment and attempts to tackle climate change.", pdf:"PDFs/array_1.pdf"},
+    {id: 2, title: "Kākāpō: Rescued From the Brink of Extinction", author: "Alison Ballance", year: 2018, price: 49.99, category: 1, image_url: "Images/Kakapo_72.jpg", description: "New Zealand's threatened night parrot, the kākāpō, has been the focus of a remarkable conservation effort that has seen the bird's population rise from a perilous low of 51 aging birds to three times that number. Kākāpō are unique and unusual birds. Long-lived, flightless heavyweights, they only breed every two to four years, and survive on just a small number of predator-free island sanctuaries.", pdf:"PDFs/array_2.pdf"}, 
+    {id: 3, title: "Geckos & Skinks: The Remarkable Lizards of Aotearoa", author: "Anna Yeoman", year: 2024, price: 59.99, category: 1, image_url: "Images/Geckos-Skinks-Final-Cover-600px-wide-72dpi.jpg", description: "The first book to tell stories about the lizards of Aotearoa New Zealand, how and where they live, and how they breed. But crucially, this book is a fascinating insight into the myriad conservation efforts that are ongoing in New Zealand, for our geckos and skinks are increasingly threatened by habitat loss and predation from pests.", pdf:"PDFs/array_3.pdf"}, 
+    {id: 4, title: "Takahē: Bird of Dreams", author: "Alison Ballance", year: 2023, price: 59.99, category: 1, image_url: "Images/TAKAHE-FC-600px-wide-72dpi.jpg", description: "The inspiring story of the rediscovery and then recovery of the takahē, one of New Zealand's most intriguing native birds. Once thought extinct, in 1948 Geoffrey Orbell found a small population living in a remote valley in the Murchison Mountains of Fiordland.", pdf:"PDFs/array_4.pdf"}, 
+    {id: 5, title: "Moa: The Life and Death of New Zealand's Legendary Bird", author: "Quinn Berentson", year: 2012, price: 59.99, category: 1, image_url: "Images/MOA-Cover-600-px-wide.jpg", description: "The moa were the most unusual and unique family of birds that ever lived, a clan of feathered monsters that developed in isolation for many, many millions of years. They became extinct reasonably quickly after the arrival of the Maori, and were a distant memory by the time European explorers arrived.", pdf:"PDFs/array_5.pdf"}, 
+    {id: 6, title: "Field Guide to New Zealand's Native Trees", author: "John Dawson & Rob Lucas", year: 2020, price: 49.99, category: 2, image_url: "Images/54e3dcff08ffb_fieldguidetontcvr72dpi.jpg", description: "This comprehensive field guide to New Zealand's native trees is compact enough to fit in a day-pack. Illustrated with more than 1500 superb photographs, it contains detailed information on all native trees found on the main islands of New Zealand.", pdf:"PDFs/array_6.pdf"}, 
+    {id: 7, title: "Fungi of Aotearoa: A Curious Forager's Field Guide", author: "Liv Sasson", year: 2023, price: 44.99, category: 2, image_url: "Images/9781761047879__00217.jpg", description: "The complete introduction to finding, identifying and using mushrooms and other fungi in New Zealand. Discover how to identify the best edible varieties, and how to cook with them, how these incredible organisms have shaped the world as we know it, and the role they are playing in modern medical and environmental research.", pdf:"PDFs/array_7.pdf"}, 
+    {id: 8, title: "The Forager's Treasury: The Essential Guide to Finding and Using Wild Plants in Aotearoa", author: "Johanna Knox", year: 2021, price: 49.99, category: 2, image_url: "Images/The_20Forager_s_20Treasury.webp", description: "In the urban and rural wildernesses, there is an abundance of food just waiting to be discovered, if only you know what to look for. Foraged food is healthy, economical and sustainable, but the best part is the fun you will have finding it. This book is guaranteed to make you look at the plants around you in a different light.", pdf:"PDFs/array_8.pdf"}, 
+    {id: 9, title: "Mystical Mushrooms: Discover the Magic and Folklore of Fantastic Fungi", author: "Aurora Kane", year: 2023, price: 40.99, category: 2, image_url: "Images/9781631069215.jpg", description: "Defines the beauty of mushrooms by focusing on their magical connections and symbolic meanings through folkloric tales and superstitions throughout the world. Explore the magical properties, mythological connections, and symbolic qualities of ever-intriguing fungi.", pdf:"PDFs/array_9.pdf"}, 
+    {id: 10, title: "Know Your New Zealand Native Plants", author: "Lawrie Metcalf", year: 2009, price: 39.99, category: 2, image_url: "Images/9781776940363.jpg", description: "Introduces a selection of the most interesting, iconic and important New Zealand plants, spanning latitudes from subtropical to temperate, and altitudes from sea level to alpine mountaintop, describing their natural history and distinguishing features.", pdf:"PDFs/array_10.pdf"},
+    {id: 11, title: "Vanishing Ice: Stories of New Zealand's Glaciers", author: "Lynley Hargreaves", year: 2022, price: 59.99, category: 3, image_url: "Images/VANISHING-ICE-cover-600px-wide-72dpi.jpg", description: "This important book tells the stories of our glaciers through the lens of human interaction, with chapters moving through time from first Māori discoverers to colonial explorers, mountaineers and modern glaciologists.", pdf:"PDFs/array_11.pdf"},
+    {id: 12, title: "Kahurangi: The Nature of Kahurangi National Park and Northwest Nelson", author: "Dave Hansford", year: 2024, price: 79.99, category: 3, image_url: "Images/Kahurangi-Cover-600px-wide-72dpi.jpg", description: "Energised by ancient, complex geology and a multitude of habitats, from vast beech stands to lush coastal rainforest, from sprawling ramparts of karst and marble to extensive wetlands and estuaries, this region holds the greatest variety of plants and animals in the country.", pdf:"PDFs/array_12.pdf"},
+    {id: 13, title: "Tamatea Dusky: The Remarkable Story of Fiordland's Dusky Sound", author: "Peta Carey", year: 2020, price: 69.99, category: 3, image_url: "Images/Tamatea-Dusky-600px.jpg", description: "The remarkable story of conservation and history in Fiordland's Tamatea/Dusky Sound, where ground-breaking work in predator control and species conservation has taken place over the last 50 years.", pdf:"PDFs/array_13.pdf"},
+    {id: 14, title: "New Zealand North and South", author: "Craig Potton", year: 2011, price: 14.99, category: 3, image_url: "Images/54e3db1449caa_nznorthandsouth.jpg", description: "New Zealand's North and South islands are remarkable for the diversity of their landscapes and culture. The essence of the South Island lies in its wild and unspoiled mountains, glaciers, forests, rivers, lakes and fiords. The North Island, a place more modified by human hands, still surprises with the beauty of its kauri forests, steaming vents and volcanoes, and the strength of its Maori cultural traditions.", pdf:"PDFs/array_14.pdf"},
+    {id: 15, title: "Not Set in Stone: The Passion and Consequence of a Mountain Life", author: "Dave Vass", year: 2023, price: 39.99, category: 3, image_url: "Images/Not-Set-In-Stone-cover-600-px-wide.jpg", description: "Between the 1980s and 2015, Dave Vass became one of New Zealand's leading mountaineers. Here he recounts the beginning of his outdoor life caving and rafting, before turning to climbing and the hard routes on Mt Aspiring National Park and Darran Mountains in Fiordland.", pdf:"PDFs/array_15.pdf"},
+    ];
 
-//Normally data is stored in DB, when app sends a request to DB, it return this array of data
-let categories = [ {id: 1, category: "Tragedy"},
-	{id: 2, category: "Crime"},
-	{id: 3, category: "Action"},
-	{id: 4, category: "Fantasy"}
-];
-        
-//Normally data is stored in DB, when app sends a request to DB, it return this array of data
-//Link: https://www.imdb.com/chart/top/
-let votes =[{id: 1, like: 1345, dislike: 10},
-    {id: 2, like: 5432, dislike: 11},
-    {id: 3, like: 7895, dislike: 15},
-    {id: 4, like: 9287, dislike: 3},
-    {id: 5, like: 9978, dislike: 2},
-    {id: 6, like: 19002, dislike: 0},
-    {id: 7, like: 2020, dislike: 16},
-    {id: 8, like: 3241, dislike: 12},
-    {id: 9, like: 2029, dislike: 10},
-    {id: 10, like: 9109, dislike: 18},
-    {id: 11, like: 10982, dislike: 23},
-    {id: 12, like: 7860, dislike: 12},
-    {id: 13, like: 8965, dislike: 21},
-    {id: 14, like: 7836, dislike: 8},
-    {id: 15, like: 1563, dislike: 4},
-    {id: 16, like: 8973, dislike: 1}
-];
+let categories = [ {id: 1, category: "Fauna"},
+    {id: 2, category: "Flora"},
+    {id: 3, category: "Terra"},  
+    ];
 
-//Normally data is stored in DB, when app sends a request to DB, it return this array of data
-//Link: https://www.imdb.com/chart/top/
-let comments =[ {id: 1, comments: [{user: "John", comment:"Good Movie"}, {user: "Ian", comment:"Not bad"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 2, comments: [{user: "Graham", comment:"Great Movie"}, {user: "Noor", comment:"Very interesting"},{user: "Kim", comment:"Excellent movie"}]},
-    {id: 3, comments: [{user: "John", comment:"Good Movie"}, {user: "Ian", comment:"Not bad"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 4, comments: [{user: "Graham", comment:"Great Movie"}]},
-    {id: 5, comments: [{user: "Ian", comment:"Not bad"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 6, comments: [{user: "Kim", comment:"Excellent movie"}]},
-    {id: 7, comments: [ {user: "Ian", comment:"Must watch"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 8, comments: [{user: "Noor", comment:"Very interesting"}]},
-    {id: 9, comments: [{user: "John", comment:"Good Movie"}]},
-    {id: 10, comments: [{user: "Noor", comment:"Great movie, recommended"},{user: "Kim", comment:"Excellent movie"}]},
-    {id: 11, comments: [{user: "John", comment:"Good Movie"}, {user: "Ian", comment:"Not bad"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 12, comments: [{user: "Noor", comment:"Very interesting"},{user: "Kim", comment:"Excellent movie"}]},
-    {id: 13, comments: [{user: "John", comment:"Good Movie"}, {user: "Ian", comment:"Not bad"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 14, comments: [ {user: "Noor", comment:"Very interesting"},{user: "Kim", comment:"Excellent movie"}, {user: "Graham", comment:"Great Movie"}]},
-    {id: 15, comments: [{user: "John", comment:"Good Movie"}, {user: "Ian", comment:"Not bad"},{user: "Daniel", comment:"Recommended"}]},
-    {id: 16, comments: [{user: "Kim", comment:"Excellent movie"}, {user: "Graham", comment:"Great Movie"}, {user: "Noor", comment:"Very interesting"}]},
-];     
+
+//Overlay menu
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
